fb45d0bc403940e8653d0e37a83b0709f2fe7834
Jojo <J.Kraken24@hotmail.com>
2021-07-02 16:46:32 +0200

    edit routing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,28 +1,40 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Order;
+import com.example.demo.service.OrderService;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication<OrderService> {
+    @Autowired
+    OrderService orderService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/ordersAll")
-    public String getOrder() {
-        return String.format("getOrder!");
+    public ResponseEntity<Object> getOrder() {
+        return new ResponseEntity<>(orderService.getOrders(), HttpStatus.OK);
     }
 
     @PostMapping("/order")
-    public String createOrder(@RequestParam(value = "clientID") int clientID, @RequestParam(value = "number") int number) {
-        return String.format("Hello %s! number: %s", clientID, number);
+    public ResponseEntity<Object>  createOrder(@RequestParam(value = "clientID") int clientID, @RequestParam(value = "number") int number) {
+        orderService.createOrder(clientID, number);
+        return new ResponseEntity<>("Order is created successsfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/order")
+    public ResponseEntity<Object>  deleteOrder(@RequestParam(value = "dountID") int dountID) {
+        orderService.deleteOrder(dountID);
+        return new ResponseEntity<>("Order is created successsfully", HttpStatus.OK);
     }
 
 }
