555f2abd12f7717286ec25a1d6b015dc29820a2b
Jojo <J.Kraken24@hotmail.com>
2021-07-02 16:45:55 +0200

    edit model order

--- src/main/java/com/example/demo/model/order.java
+++ src/main/java/com/example/demo/model/order.java
@@ -1,12 +1,17 @@
 package com.example.demo.model;
 
 public class Order {
+    private int id;
     private int clientID;
     private int number;
 
-    public Order (int clientID, int number) {
+    public Order(int clientID, int number) {
         this. number = number;
         this.clientID = clientID;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getClientID() {
@@ -15,6 +20,10 @@ public class Order {
 
     public int getNumber() {
         return number;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setClientID(int clientID) {
